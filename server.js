4808ba146503fd44ef592db643102fd945cb9834
Nicky Pike <npike@npike-a01.vmware.com>
2022-04-27 10:45:27 -0600

    update to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  res.send('Hello Jay.  From Nicky!!');
 });
 
 app.get('/health', (req, res) => {
